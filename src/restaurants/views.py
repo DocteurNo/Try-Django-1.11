8954c1f495fb23c015bc7fd1ac771961fe5bfbb3
DocteurNo <stanpub49-poubelle@yahoo.fr>
2018-04-08 12:38:24 +0200

    8 - Context in Django Templates

---
+++ src/restaurants/views.py
@@ -0,0 +1,19 @@
+import random
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def home(request):
+	num = None
+	some_list = [
+		random.randint(0, 100000),
+		random.randint(0, 100000),
+		random.randint(0, 100000)
+	]
+	condition_bool_item = False
+	if condition_bool_item:
+		num = random.randint(0, 100000)
+	context = {
+		"num": num,
+		"some_list": some_list
+	}
+	return render(request, "base.html", context)# 1h08 39s
