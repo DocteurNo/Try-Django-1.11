1047aa28394bb02a3f3427aa56eb2b3d8d2af7c3
DocteurNo <stanpub49-poubelle@yahoo.fr>
2018-04-09 10:25:54 +0200

    16 - Displaying Saved Data

--- src/restaurants/models.py
+++ src/restaurants/models.py
@@ -7,3 +7,6 @@ class RestaurantLocation(models.Model):
 	timestamp 	= models.DateTimeField(auto_now_add=True)
 	updated 	= models.DateTimeField(auto_now=True)
 	# my_date		= models.DateTimeField(auto_now=False, auto_now_add=False)
+
+	def __str__(self):
+		return self.name
--- src/restaurants/views.py
+++ src/restaurants/views.py
@@ -1,25 +1,14 @@
-import random
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.views import View
 from django.views.generic.base import TemplateView
 
-class HomeView(TemplateView):
-	template_name = "home.html"
+from .models import RestaurantLocation
 
-	def get_context_data(self, *args, **kwargs):
-		context = super(HomeView, self).get_context_data(*args, **kwargs)
-		num = None
-		some_list = [
-			random.randint(0, 100000),
-			random.randint(0, 100000),
-			random.randint(0, 100000)
-		]
-		condition_bool_item = False
-		if condition_bool_item:
-			num = random.randint(0, 100000)
-		context = {
-			"num": num,
-			"some_list": some_list
-		}
-		return context
+def restaurant_listview(request):
+	template_name = 'restaurants/restaurants_list.html'
+	queryset = RestaurantLocation.objects.all()
+	context = {
+		"object_list": queryset
+	}
+	return render(request, template_name, context)
