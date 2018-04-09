42eaf906557fee99adf6c2eecadc22629231b8bb
DocteurNo <stanpub49-poubelle@yahoo.fr>
2018-04-09 19:10:26 +0200

    23 - Get Single Items from the DB

--- src/restaurants/views.py
+++ src/restaurants/views.py
@@ -6,30 +6,38 @@ from django.views.generic import TemplateView, ListView, DetailView
 
 from .models import RestaurantLocation
 
-def restaurant_listview(request):
-	template_name = 'restaurants/restaurants_list.html'
-	queryset = RestaurantLocation.objects.all()
-	context = {
-		"object_list": queryset
-	}
-	return render(request, template_name, context)
+def restaurant_listview(request,):
+    template_name = 'restaurants/restaurants_list.html'
+    queryset = RestaurantLocation.objects.all()
+    context = {
+        "object_list": queryset
+    }
+    return render(request, template_name, context)
+
+def restaurant_detailview(request, slug):
+    template_name = 'restaurants/restaurantlocation_detail.html'
+    obj = RestaurantLocation.objects.get(slug=slug)
+    context = {
+        "object": obj
+    }
+    return render(request, template_name, context)
 
 class RestaurantListView(ListView):
-	def get_queryset(self):
-		slug = self.kwargs.get("slug")
-		if slug:
-			queryset = RestaurantLocation.objects.filter(
-				Q(category__iexact=slug) | 
-				Q(category__icontains=slug)
-			)
-		else:
-			queryset = RestaurantLocation.objects.all()
-		return queryset
+    def get_queryset(self):
+        slug = self.kwargs.get("slug")
+        if slug:
+            queryset = RestaurantLocation.objects.filter(
+                    Q(category__iexact=slug) |
+                    Q(category__icontains=slug)
+                )
+        else:
+            queryset = RestaurantLocation.objects.all()
+        return queryset
 
 class RestaurantDetailView(DetailView):
-	queryset = RestaurantLocation.objects.all()
-
-	# def get_object(self, *args, **kwargs):
-	# 	rest_id = self.kwargs.get("rest_id")
-	# 	obj = get_object_or_404(RestaurantLocation, id=rest_id)# pk = rest_id
-	# 	return obj
+    queryset = RestaurantLocation.objects.all() #.filter(category__iexact='mexican')
+    
+    # def get_object(self, *args, **kwargs):
+    #     rest_id = self.kwargs.get('rest_id')
+    #     obj = get_object_or_404(RestaurantLocation, id=rest_id) # pk = rest_id
+    #     return obj
