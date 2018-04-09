4319cd7268cf89d86df754b720b29f8c398b0b01
DocteurNo <stanpub49-poubelle@yahoo.fr>
2018-04-09 20:57:40 +0200

    24 - Saving Data the Hard + Wrong Way

--- src/restaurants/views.py
+++ src/restaurants/views.py
@@ -1,12 +1,12 @@
 from django.db.models import Q
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 from django.views import View
 from django.views.generic import TemplateView, ListView, DetailView
 
 from .models import RestaurantLocation
 
-def restaurant_listview(request,):
+def restaurant_listview(request):
     template_name = 'restaurants/restaurants_list.html'
     queryset = RestaurantLocation.objects.all()
     context = {
@@ -21,6 +21,21 @@ def restaurant_detailview(request, slug):
         "object": obj
     }
     return render(request, template_name, context)
+
+def restaurant_createview(request):
+	if request.method == 'POST':
+		title = request.POST.get("title")
+		location = request.POST.get("location")
+		category = request.POST.get("category")
+		obj = RestaurantLocation.objects.create(
+			name = title,
+			location = location,
+			category = category
+		)
+		return HttpResponseRedirect('/restaurants')
+	template_name = 'restaurants/form.html'
+	context = {}
+	return render(request, template_name, context)
 
 class RestaurantListView(ListView):
     def get_queryset(self):
