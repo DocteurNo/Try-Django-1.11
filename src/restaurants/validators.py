726a70c9d231906e5718bd97398470e2a6e763de
DocteurNo <stanpub49-poubelle@yahoo.fr>
2018-04-12 17:14:22 +0200

    38 - User Profile View

--- src/restaurants/validators.py
+++ src/restaurants/validators.py
@@ -1,27 +1,20 @@
 from django.core.exceptions import ValidationError
-from django.utils.translation import ugettext_lazy as _
 
 def validate_even(value):
     if value % 2 != 0:
         raise ValidationError(
-            _('%(value)s is not an even number'),
+            '%(value)s is not an even number',
             params={'value': value},
         )
 
 def validate_email(value):
-    email = value
-    if ".edu" in email:
-        raise ValidationError(_("We do not accept edu emails"))
+        email = value
+        if ".edu" in email:
+            raise ValidationError("We do not accept edu emails")
 
-CATEGORIES = ['Mexican', 'Fast Food', 'Gastronomic']
+CATEGORIES = ['Mexican', 'Asian', 'American', 'Italian', 'Chinese', 'Thai', 'Pizza', 'Other']
 
 def validate_category(value):
-	cat = value.capitalize()
-	if not value in CATEGORIES and not cat in CATEGORIES:
-		#raise ValidationError(_(f"{value} not a category valid"))
-		raise ValidationError(
-		    _('%(value)s not a category valid'),
-		    code='invalid',
-		    params={'value': value},
-		)
-		# raise ValidationError(_('Invalid value: %s') % value)
+    cat = value.capitalize()
+    if not value in CATEGORIES and not cat in CATEGORIES:
+        raise ValidationError(f"{value} not a valid category")
