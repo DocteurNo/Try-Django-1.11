ed14e6e1960bac4ffc901305ba0b8bb8d8923712
DocteurNo <stanpub49-poubelle@yahoo.fr>
2018-04-12 10:37:10 +0200

    36 - Limiting Form Field to QuerySet

--- src/menus/views.py
+++ src/menus/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView, CreateView, UpdateView
 
@@ -15,7 +16,7 @@ class ItemDetailView(DetailView):
         return Item.objects.filter(user=self.request.user)
 
 
-class ItemCreateView(CreateView):
+class ItemCreateView(LoginRequiredMixin, CreateView):
     template_name = 'form.html'
     form_class = ItemForm
 
@@ -27,19 +28,29 @@ class ItemCreateView(CreateView):
     def get_queryset(self):
         return Item.objects.filter(user=self.request.user)
 
+    def get_form_kwargs(self):
+        kwargs = super(ItemCreateView, self).get_form_kwargs()
+        kwargs['user'] = self.request.user
+        return kwargs
+
     def get_context_data(self, *args, **kwargs):
         context = super(ItemCreateView, self).get_context_data(*args, **kwargs)
         context['title'] = 'Create Item'
         return context
 
 
-class ItemUpdateView(UpdateView):
+class ItemUpdateView(LoginRequiredMixin, UpdateView):
     template_name = 'form.html'
     form_class = ItemForm
 
     def get_queryset(self):
         return Item.objects.filter(user=self.request.user)
 
+    def get_form_kwargs(self):
+        kwargs = super(ItemUpdateView, self).get_form_kwargs()
+        kwargs['user'] = self.request.user
+        return kwargs
+    
     def get_context_data(self, *args, **kwargs):
         context = super(ItemUpdateView, self).get_context_data(*args, **kwargs)
         context['title'] = 'Update Item'
